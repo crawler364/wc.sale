95171f7e8a3eb9e928f98ef423239dafd39becad
crawler364 <crawler364@gmail.com>
2020-12-15 13:35:29 +0300

    1723.28

--- include.php
+++ include.php
@@ -1,6 +1,7 @@
 <?php
 
 Bitrix\Main\Loader::includeModule('sale');
+Bitrix\Main\Loader::includeModule('wc.main');
 
 $registry = Bitrix\Sale\Registry::getInstance(Bitrix\Sale\Registry::REGISTRY_TYPE_ORDER);
 $registry->set(Bitrix\Sale\Registry::ENTITY_BASKET, WC\Sale\Basket::class);
