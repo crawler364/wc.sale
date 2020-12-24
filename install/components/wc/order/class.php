3daf1ce6a960429b752f6fd1e0fac24540439bc4
crawler364 <crawler364@gmail.com>
2020-12-24 17:56:53 +0300

    sale 0.0.6

---
+++ install/components/wc/order/class.php
@@ -0,0 +1,9 @@
+<?php
+
+class WCSaleOrder extends CBitrixComponent implements Bitrix\Main\Engine\Contract\Controllerable
+{
+    public function configureActions()
+    {
+        // TODO: Implement configureActions() method.
+    }
+}
