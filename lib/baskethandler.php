4fc97433ca25de5e08cf17fdc213c16a5293885b
crawler364 <crawler364@gmail.com>
2020-12-31 15:34:33 +0300

    1723.72

--- lib/baskethandler.php
+++ lib/baskethandler.php
@@ -4,22 +4,111 @@
 namespace WC\Sale;
 
 
+use Bitrix\Main\Loader;
 use WC\Main\Messages;
 use WC\Main\Result;
 
 class BasketHandler
 {
     /**
+     * @var BasketItem $basketItem
      * @var Basket $basket
      */
+    public $basketItem;
     public $basket;
+    public $productProvider = \CCatalogProductProvider::class;
+    //protected Basket|BasketItem $object; Union types are available in PHP 8.0 only
 
-    public function __construct(Basket $basket)
+    public function __construct($object)
     {
         $this->result = new Result();
         $this->mess = new Messages(__FILE__);
 
-        $this->basket = $basket;
+        if ($object instanceof BasketItem) {
+            $this->basketItem = $object;
+            $this->basket = $object->getCollection();
+        } elseif ($object instanceof Basket) {
+            $this->basket = $object;
+        } else {
+            throw new \Bitrix\Main\ArgumentTypeException($object);
+        }
+    }
+
+    public function processBasketItem($action, $quantity = null)
+    {
+        /** @var \Bitrix\Main\Result $r */
+
+        if ($action != 'set') {
+            $quantity = $this->basketItem->mathQuantity($action);
+        }
+
+        $this->quantity = $this->basketItem->checkQuantity($quantity);
+
+        if ($this->quantity > 0) {
+            if ($this->basketItem->getId() == null) {
+                $this->addBasketItem();
+            } else {
+                $this->updateBasketItem();
+            }
+        } else {
+            $this->deleteBasketItem();
+        }
+
+        $r = $this->basket->save();
+        $this->result->mergeResult($r);
+
+        if ($this->result->isSuccess()) {
+            $this->result->setData([
+                'ITEM' => $this->basketItem->getInfo(),
+                'BASKET' => $this->basket->getInfo(),
+            ]);
+        }
+
+        // todo $this->result += данные по доставке?
+
+        return $this->result;
+    }
+
+    protected function addBasketItem()
+    {
+        $this->basketItem->setField('PRODUCT_PROVIDER_CLASS', $this->productProvider);
+
+        $this->basketItem->setField('QUANTITY', $this->quantity);
+
+        $fields = $this->basketItem->prepareBasketItemFields();
+        $this->basketItem->setFields($fields);
+
+        $this->basketItem->setPriceName();
+
+        $this->basketItem->setPropertyArticle();
+    }
+
+    protected function updateBasketItem()
+    {
+        $this->basketItem->setField('QUANTITY', $this->quantity);
+    }
+
+    protected function deleteBasketItem()
+    {
+        $this->basketItem->delete();
+    }
+
+    /**
+     * @param $productId
+     * @param Basket|null $basket
+     * @return BasketItem|null
+     */
+    public static function getBasketItem($productId, Basket $basket = null): ?BasketItem
+    {
+        Loader::includeModule('catalog');
+        $basket = $basket ?: self::getCurrentUserBasket();
+        if (!$basketItem = $basket->getItemBy(['PRODUCT_ID' => $productId])) {
+            if (\Bitrix\Catalog\ProductTable::getById($productId)->fetch()) {
+                $basketItem = $basket->createItem('catalog', $productId);
+            }
+        }
+
+        return $basketItem;
     }
 
     public static function getCurrentUserBasket(): Basket
