ce1dad079b230cff85c3c3657494c03eb93e914a
crawler364 <crawler364@gmail.com>
2021-01-02 15:24:38 +0300

    sale 0.0.11

--- install/components/wc/basket/templates/.default/script.js
+++ install/components/wc/basket/templates/.default/script.js
@@ -1,10 +1,42 @@
 class WCSaleBasket {
-    constructor(param){
-        BX.bind(BX('test'), "click", this.onMoveRightSlider());
+    constructor() {
+        let productId, actionObjects, action, actionData, on, basket, products;
+
+        basket = BX('basket');
+        products = BX.findChild(basket, {
+                'tag': 'div',
+                'attribute': 'data-basket-product-id',
+            },
+            true,
+            true,
+        );
+
+        products.forEach((product) => {
+            productId = product.getAttribute('data-basket-product-id');
+
+            actionObjects = BX.findChild(product, {
+                    'attribute': 'data-basket-action',
+                },
+                true,
+                true,
+            );
+
+            actionObjects.forEach((actionObject) => {
+                action = actionObject.getAttribute('data-basket-action');
+                actionData = {'productId': productId, 'action': action};
+
+                if (action == 'set') {
+                    on = 'blur';
+                } else {
+                    on = 'click';
+                }
+
+                BX.bind(actionObject, on, BX.delegate(this.action.bind(this, actionData), this));
+            });
+        });
     }
 
-    onMoveRightSlider(){
-        console.log(123);
-
+    action(param) {
+        console.log(param);
     }
 }
--- install/components/wc/basket/templates/.default/template.php
+++ install/components/wc/basket/templates/.default/template.php
@@ -3,8 +3,24 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
 ?>
+<div id="basket">
+  <div data-basket-product-id="123">
+    <div class='lol' data-basket-action="plus">+</div>
+    <input type="text" data-basket-action="set">
+    <div data-basket-action="minus">-</div>
+    <div data-basket-action="delete">X</div>
+  </div>
 
-<div class="test">TEST</div>
+  <div data-basket-product-id="124">
+    <div class='lol' data-basket-action="plus">+</div>
+    <input type="text" data-basket-action="set">
+    <div data-basket-action="minus">-</div>
+    <div data-basket-action="delete">X</div>
+  </div>
+
+
+</div>
+
 <script type="text/javascript">
     WCSaleBasket = new WCSaleBasket();
 
