54d4277ca4c19a2bac821b26e64e928ac02c4e27
crawler364 <crawler364@gmail.com>
2021-01-02 15:24:10 +0300

    sale 0.0.10

---
+++ install/components/wc/basket/templates/.default/script.js
@@ -0,0 +1,10 @@
+class WCSaleBasket {
+    constructor(param){
+        BX.bind(BX('test'), "click", this.onMoveRightSlider());
+    }
+
+    onMoveRightSlider(){
+        console.log(123);
+
+    }
+}
--- install/components/wc/basket/templates/.default/template.php
+++ install/components/wc/basket/templates/.default/template.php
@@ -1,1 +1,12 @@
 <?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+?>
+
+<div class="test">TEST</div>
+<script type="text/javascript">
+    WCSaleBasket = new WCSaleBasket();
+
+
+</script>
