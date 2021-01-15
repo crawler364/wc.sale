09b2de1393b04fb6104e04ea990ba24eb303d484
crawler364 <crawler364@gmail.com>
2021-01-15 11:15:06 +0300

    sale 0.1.12 basket

---
+++ install/components/wc/basket/ajax.php
@@ -0,0 +1,44 @@
+<?php
+
+use Bitrix\Main\Engine\Response\AjaxJson;
+use WC\Main\Result;
+
+class WCSaleBasketController extends \Bitrix\Main\Engine\Controller
+{
+    /** @var \WC\Sale\BasketHandler */
+    private $basketHandlerClass = \WC\Sale\BasketHandler::class;
+
+    public function configureActions(): array
+    {
+        return [
+            'process' => [
+                'prefilters' => [], 'postfilters' => [],
+            ],
+        ];
+    }
+
+    public function processAction(string $basketAction, array $product, $basketHandlerClass = null): AjaxJson
+    {
+        $this->result = new Result();
+
+        $basketHandlerClass = $basketHandlerClass ?: $this->basketHandlerClass;
+
+        if (!$basketItem = $basketHandlerClass::getBasketItem($product['id'])) {
+            $this->result->addError('WC_UNDEFINED_PRODUCT');
+        } else {
+            $basketHandler = new $basketHandlerClass($basketItem);
+            $basketHandler->processBasketItem($basketAction, $product['quantity']);
+            $this->result = $basketHandler->saveBasket();
+        }
+
+        if ($this->result->isSuccess()) {
+            $basket = $basketHandlerClass::getCurrentUserBasket();
+            $this->result->setData([
+                'BASKET_ITEM' => $basketItem->getInfo(),
+                'BASKET' => $basket->getInfo(),
+            ]);
+        }
+
+        return $this->result->prepareAjaxJson();
+    }
+}
--- install/components/wc/basket/class.php
+++ install/components/wc/basket/class.php
@@ -1,10 +1,8 @@
 <?php
 
-use Bitrix\Main\Engine\Response\AjaxJson;
 use Bitrix\Main\Loader;
-use WC\Main\Result;
 
-class WCSaleBasket extends CBitrixComponent implements Bitrix\Main\Engine\Contract\Controllerable
+class WCSaleBasket extends CBitrixComponent
 {
     /** @var \WC\Sale\BasketHandler */
     private $basketHandlerClass = \WC\Sale\BasketHandler::class;
@@ -16,48 +14,14 @@ class WCSaleBasket extends CBitrixComponent implements Bitrix\Main\Engine\Contra
         Loader::includeModule('wc.sale');
     }
 
-    public function configureActions(): array
-    {
-        return [
-            'process' => [
-                'prefilters' => [], 'postfilters' => [],
-            ],
-        ];
-    }
-
-    public function processAction(string $basketAction, array $product, $basketHandlerClass = null): AjaxJson
-    {
-        $this->result = new Result();
-
-        $basketHandlerClass = $basketHandlerClass ?: $this->basketHandlerClass;
-
-        if (!$basketItem = $basketHandlerClass::getBasketItem($product['id'])) {
-            $this->result->addError('WC_UNDEFINED_PRODUCT');
-        } else {
-            $basketHandler = new $basketHandlerClass($basketItem);
-            $basketHandler->processBasketItem($basketAction, $product['quantity']);
-            $this->result = $basketHandler->saveBasket();
-        }
-
-        if ($this->result->isSuccess()) {
-            $basket = $basketHandlerClass::getCurrentUserBasket();
-            $this->result->setData([
-                'BASKET_ITEM' => $basketItem->getInfo(),
-                'BASKET' => $basket->getInfo(),
-            ]);
-        }
-
-        return $this->result->prepareAjaxJson();
-    }
-
     public function executeComponent()
     {
         \CUtil::InitJSCore(['ajax', 'wc.sale.basket']);
 
-        $this->basketHandlerClass = $this->arParams['BASKET_HANDLER_CLASS'] ?: $this->basketHandlerClass;
-        $this->basket = $this->basketHandlerClass::getCurrentUserBasket();
-        $this->basketItems = $this->basket->getBasketItems();
-        $this->arResult = $this->basket->getInfo();
+        $basketHandlerClass = $this->arParams['BASKET_HANDLER_CLASS'] ?: $this->basketHandlerClass;
+        $basket = $basketHandlerClass::getCurrentUserBasket();
+
+        $this->arResult = $basket->getInfo();
 
         $this->includeComponentTemplate();
     }
