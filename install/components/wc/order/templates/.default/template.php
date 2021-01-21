f44a8db1b76e6bcb120fe96dcb788e8fe7a4f63b
crawler364 <crawler364@gmail.com>
2021-01-21 19:59:29 +0300

    sale 0.3.13

--- install/components/wc/order/templates/.default/template.php
+++ install/components/wc/order/templates/.default/template.php
@@ -2,10 +2,8 @@
 
 use Bitrix\Main\Localization\Loc;
 
-$a = $arResult;
-
 ?>
-<form action="" method="post">
+<form id="wc-order" action="" method="post">
     <h2><?= Loc::getMessage('WC_ORDER_PERSON_TYPE') ?></h2>
     <div>
         <? foreach ($arResult['PERSON_TYPES'] as $personType) { ?>
@@ -17,9 +15,33 @@ $a = $arResult;
     </div>
 
     <h2><?= Loc::getMessage('WC_ORDER_TEMPLATE_PROPERTIES_TITLE') ?></h2>
-    <? foreach ($arResult['PROPERTIES'] as $property) { ?>
-        <?=$property['NAME']?>
+    <table>
+        <? foreach ($arResult['PROPERTIES'] as $property) { ?>
+            <tr>
+                <td>
+                    <label for="<?= $property['CODE'] ?>">
+                        <?= $property['NAME'] ?><br>
+                    </label>
+                </td>
+                <td>
+                    <? switch ($arProp['TYPE']) {
+                        case 'LOCATION':
+                        default: ?>
+                            <input id="<?= $property['CODE'] ?>" type="text" name="<?= $property['CODE'] ?>"
+                                   value="<?= $property['VALUE'][0] ?>">
+                        <? } ?>
+                </td>
+            </tr>
+        <? } ?>
+    </table>
 
-    <? } ?>
-
+    <button type="submit"><?= Loc::getMessage('WC_ORDER_SUBMIT_BUTTON') ?></button>
 </form>
+
+<script type="text/javascript">
+    if (!window.hasOwnProperty('WCSaleOrder')) {
+        window.WCSaleOrder = new WCSaleOrder(<?=Bitrix\Main\Web\Json::encode([
+        ])?>);
+        window.WCSaleOrder.init();
+    }
+</script>
