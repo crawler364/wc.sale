40285470edd8733b57a55bd456dc8ccc9141e6c7
crawler364 <crawler364@gmail.com>
2021-02-05 16:28:28 +0300

    sale 0.4.8

--- install/components/wc/basket/templates/.default/component_epilog.php
+++ install/components/wc/basket/templates/.default/component_epilog.php
@@ -1,3 +1,6 @@
 <?php
 
-Bitrix\Main\Page\Asset::getInstance()->addJs("$templateFolder/domhandler.js");
+if (!defined('WC_SALE_BASKET_DOM_HANDLER')) {
+    define('WC_SALE_BASKET_DOM_HANDLER', TRUE);
+    Bitrix\Main\Page\Asset::getInstance()->addJs("$templateFolder/domhandler.js");
+}
